f5eae19ca8fab03ba085aa1b76de3917b646749b
EunhyangKim <galaxybob19@gmail.com>
2016-05-06 16:16:26 +0900

    add enqueue/dequeue function prototype
    add enqueue/dequeue function prototype

--- linux/kernel/sched/wrr.c
+++ linux/kernel/sched/wrr.c
@@ -1,46 +1,72 @@
-#include "sched.h"
-#include <linux/slab.h>
-
 /*
 	Implementation for weighted-round-robin policy.	
 */
 
+#include "sched.h"
+#include <linux/slab.h>
+
+
 /*TODO: rearrange methods we nee */
 
-const struct sched_class fair_sched_class = {
-	.next			= &idle_sched_class,      /* modify to fair_sched_class */
-	.enqueue_task		= enqueue_task_fair,
-	.dequeue_task		= dequeue_task_fair,
-	.yield_task		= yield_task_fair,
-	.yield_to_task		= yield_to_task_fair,
+const struct sched_class wrr_sched_class;
+
+static void enqueue_task_wrr(struct rq *rq, struct task_struct *p, int flag)
+{
+	struct wrr_rq *wrr_rq;
+	struct sched_wrr_entity *wrr = &p->wrr;
+
+}
+
+static void dequeue_task_wrr(struct rq *rq, struct task_struct *p)
+{
+}
+
+static void yield_task_wrr()
+{
+}
+
+static void yield_to_task_wrr()
+{
+}
+
+
+
+const struct sched_class wrr_sched_class = {
+	.next			= &fair_sched_class,
+	.enqueue_task		= enqueue_task_wrr,
+	.dequeue_task		= dequeue_task_wrr,
+	.yield_task		= yield_task_wrr,
+	.yield_to_task		= yield_to_task_wrr,
 
 	.check_preempt_curr	= check_preempt_wakeup,
 
-	.pick_next_task		= pick_next_task_fair,
-	.put_prev_task		= put_prev_task_fair,
+	.pick_next_task		= pick_next_task_wrr,
+	.put_prev_task		= put_prev_task_wrr,
 
 #ifdef CONFIG_SMP
-	.select_task_rq		= select_task_rq_fair,
+	.select_task_rq		= select_task_rq_wrr,
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	.migrate_task_rq	= migrate_task_rq_fair,
+	.migrate_task_rq	= migrate_task_rq_wrr,
 #endif
-	.rq_online		= rq_online_fair,
-	.rq_offline		= rq_offline_fair,
+	.rq_online		= rq_online_wrr,
+	.rq_offline		= rq_offline_wrr,
 
-	.task_waking		= task_waking_fair,
+	.task_waking		= task_waking_wrr,
 #endif
 
-	.set_curr_task          = set_curr_task_fair,
-	.task_tick		= task_tick_fair,
-	.task_fork		= task_fork_fair,
+	.set_curr_task          = set_curr_task_wrr,
+	.task_tick		= task_tick_wrr,
+	.task_fork		= task_fork_wrr,
 
-	.prio_changed		= prio_changed_fair,
-	.switched_from		= switched_from_fair,
-	.switched_to		= switched_to_fair,
+	.prio_changed		= prio_changed_wrr,
+	.switched_from		= switched_from_wrr,
+	.switched_to		= switched_to_wrr,
 
-	.get_rr_interval	= get_rr_interval_fair,
+	.get_rr_interval	= get_rr_interval_wrr,
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	.task_move_group	= task_move_group_fair,
+	.task_move_group	= task_move_group_wrr,
 #endif
 };
+
+
