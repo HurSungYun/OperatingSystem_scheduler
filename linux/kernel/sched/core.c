2abd8f7af75962f88606bebaf105ae2abd3cc45a
EunhyangKim <galaxybob19@gmail.com>
2016-05-25 01:04:41 +0900

    fix pick_next to return cursor instead of cursor's next
    fix pick_next to return cursor instead of cursor's next

--- linux/kernel/sched/core.c
+++ linux/kernel/sched/core.c
@@ -2973,7 +2973,7 @@ void scheduler_tick(void)
 #ifdef CONFIG_SMP
 	rq->idle_balance = idle_cpu(cpu);
 	trigger_load_balance(rq, cpu);
-	load_balance_wrr(rq);
+//	load_balance_wrr(rq);
 #endif
 	rq_last_tick_reset(rq);
 }
